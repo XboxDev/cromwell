ddbc37a0db9c83c79bfe4db95192c238d803a533
Oliver Schwartz <Oliver.Schwartz@gmx.de>
2004-03-27 18:49:50 +0000

    Disable tagged images for now

--- etherboot/include/etherboot_config.h
+++ etherboot/include/etherboot_config.h
@@ -4,10 +4,9 @@
 #define VERSION_MAJOR 5
 #define VERSION_MINOR 2
 #define VERSION_PATCH 4
-#define ARCH i386
 #define VERSION "5.2.4"
 #define DOWNLOAD_PROTO_TFTP
-#define TAGGED_IMAGE
+// #define DOWNLOAD_PROTO_NFS
 #define ELF_IMAGE
 #define CONFIG_PCI
 
