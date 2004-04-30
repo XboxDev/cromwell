2764ea97c9439c0083a27406ca6fb0ab2b2b9866
Oliver Schwartz <Oliver.Schwartz@gmx.de>
2004-04-30 23:16:53 +0000

    Small fixes for parsing linuxboot.cfg

--- lib/misc/BootParser.c
+++ lib/misc/BootParser.c
@@ -1,8 +1,6 @@
 #include "boot.h"
 #include <string.h>
 
-
-
 int ParseConfig(char *szBuffer, CONFIGENTRY *entry, EEPROMDATA *eeprom, char *szPath) {
 	static char szLine[MAX_LINE];
 	static char szTmp[MAX_LINE];
@@ -16,12 +14,12 @@ int ParseConfig(char *szBuffer, CONFIGENTRY *entry, EEPROMDATA *eeprom, char *sz
 	entry->nValid = 1;
 
 	while(1) {
-                memset(szLine,0x00,MAX_LINE);
-                _strncpy(szLine,ptr,MAX_LINE);
-                szLine[MAX_LINE-1]=0; 	// Make sure, we are Terminated
+		memset(szLine,0x00,MAX_LINE);
+		_strncpy(szLine,ptr,MAX_LINE);
+		szLine[MAX_LINE-1]=0; 	// Make sure string is terminated
                 
 		if(strlen(ptr) < MAX_LINE) {
-			if(_strncmp(ptr,"kernel",strlen("kernel")) == 0)  {
+			if(_strncmp(ptr,"kernel ",strlen("kernel ")) == 0)  {
 				HelpGetParm(szTmp, ptr);
 				
 				if (szPath!= NULL) 	sprintf(entry->szKernel,"%s",szPath);
@@ -31,7 +29,7 @@ int ParseConfig(char *szBuffer, CONFIGENTRY *entry, EEPROMDATA *eeprom, char *sz
 				sprintf(entry->szKernel,"%s%s",entry->szKernel,szTmp);
 			}
 			
-			if(_strncmp(ptr,"initrd",strlen("initrd")) == 0) {
+			if(_strncmp(ptr,"initrd ",strlen("initrd ")) == 0) {
 				HelpGetParm(szTmp, ptr);
 				
 				if (( szPath!= NULL) &&
@@ -52,8 +50,8 @@ int ParseConfig(char *szBuffer, CONFIGENTRY *entry, EEPROMDATA *eeprom, char *sz
 				entry->nVesaFB = 1;
 			}
 			
-			if(_strncmp(ptr,"append",strlen("append")) == 0)
-				sprintf(entry->szAppend,"%s",ptr);
+			if(_strncmp(ptr,"append ",strlen("append ")) == 0)
+				HelpGetParm(entry->szAppend, ptr);
 		} else {
 			entry->nValid = 0;
 		}
