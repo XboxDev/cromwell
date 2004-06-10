98cc81ddbb894eabf3baa1586f869254f5314ecd
Oliver Schwartz <Oliver.Schwartz@gmx.de>
2004-06-10 20:23:27 +0000

    Changed version to 2.31

--- include/consts.h
+++ include/consts.h
@@ -16,7 +16,7 @@
  *                                                                         *
  ***************************************************************************/
 
-#define VERSION "2.31-dev"
+#define VERSION "2.31"
 
 #define PCI_CFG_ADDR 0x0CF8
 #define PCI_CFG_DATA 0x0CFC
