eb7a5cc3c456cb4380af81882167eb1cd9829738
David Pye <dmp@davidmpye.dyndns.org>
2004-08-12 20:57:15 +0000

    Add hashes for 1.6 xboxes - thanks to ldots from X-S.

--- drivers/ide/BootHddKey.c
+++ drivers/ide/BootHddKey.c
@@ -35,31 +35,36 @@ int copy_swap_trim(unsigned char *dst, unsigned char *src, int len)
 int HMAC1hddReset(int version,SHA1Context *context)
 {
   	SHA1Reset(context);
-  	
-  	if (version==9) {
-  		context->Intermediate_Hash[0] = 0x85F9E51A;
-		context->Intermediate_Hash[1] = 0xE04613D2;
-		context->Intermediate_Hash[2] = 0x6D86A50C;
-		context->Intermediate_Hash[3] = 0x77C32E3C;
-		context->Intermediate_Hash[4] = 0x4BD717A4;
+	switch (version) {
+		case 9:
+  			context->Intermediate_Hash[0] = 0x85F9E51A;
+			context->Intermediate_Hash[1] = 0xE04613D2;
+			context->Intermediate_Hash[2] = 0x6D86A50C;
+			context->Intermediate_Hash[3] = 0x77C32E3C;
+			context->Intermediate_Hash[4] = 0x4BD717A4;
+  			break;
+		case 10:
+			context->Intermediate_Hash[0] = 0x72127625;
+			context->Intermediate_Hash[1] = 0x336472B9;
+			context->Intermediate_Hash[2] = 0xBE609BEA;
+			context->Intermediate_Hash[3] = 0xF55E226B;
+			context->Intermediate_Hash[4] = 0x99958DAC;
+			break;
+		case 11:
+			context->Intermediate_Hash[0] = 0x39B06E79;
+			context->Intermediate_Hash[1] = 0xC9BD25E8;
+			context->Intermediate_Hash[2] = 0xDBC6B498;
+			context->Intermediate_Hash[3] = 0x40B4389D;
+			context->Intermediate_Hash[4] = 0x86BBD7ED;
+			break;
+		case 12:
+			context->Intermediate_Hash[0] = 0x8058763a;
+                 	context->Intermediate_Hash[1] = 0xf97d4e0e;
+                   	context->Intermediate_Hash[2] = 0x865a9762;
+	               	context->Intermediate_Hash[3] = 0x8a3d920d;
+			context->Intermediate_Hash[4] = 0x08995b2c;
+			break;
 	}
-  	
-  	if (version==10) {
-		context->Intermediate_Hash[0] = 0x72127625;
-		context->Intermediate_Hash[1] = 0x336472B9;
-		context->Intermediate_Hash[2] = 0xBE609BEA;
-		context->Intermediate_Hash[3] = 0xF55E226B;
-		context->Intermediate_Hash[4] = 0x99958DAC;
-	}
-	
-	if (version==11) {
-		context->Intermediate_Hash[0] = 0x39B06E79;
-		context->Intermediate_Hash[1] = 0xC9BD25E8;
-		context->Intermediate_Hash[2] = 0xDBC6B498;
-		context->Intermediate_Hash[3] = 0x40B4389D;
-		context->Intermediate_Hash[4] = 0x86BBD7ED;
-	}
-
 	context->Length_Low = 512;
 
 	return shaSuccess;
@@ -68,37 +73,40 @@ int HMAC1hddReset(int version,SHA1Context *context)
 int HMAC2hddReset(int version,SHA1Context *context)
 {
 	SHA1Reset(context);
-	
-	if (version==9) {
-		context->Intermediate_Hash[0] = 0x5D7A9C6B;
-		context->Intermediate_Hash[1] = 0xE1922BEB;
-		context->Intermediate_Hash[2] = 0xB82CCDBC;
-		context->Intermediate_Hash[3] = 0x3137AB34;
-		context->Intermediate_Hash[4] = 0x486B52B3;
+	switch (version) {
+		case 9:
+			context->Intermediate_Hash[0] = 0x5D7A9C6B;
+			context->Intermediate_Hash[1] = 0xE1922BEB;
+			context->Intermediate_Hash[2] = 0xB82CCDBC;
+			context->Intermediate_Hash[3] = 0x3137AB34;
+			context->Intermediate_Hash[4] = 0x486B52B3;
+			break;
+		case 10:
+			context->Intermediate_Hash[0] = 0x76441D41;
+			context->Intermediate_Hash[1] = 0x4DE82659;
+			context->Intermediate_Hash[2] = 0x2E8EF85E;
+			context->Intermediate_Hash[3] = 0xB256FACA;
+			context->Intermediate_Hash[4] = 0xC4FE2DE8;
+			break;
+		case 11:
+			context->Intermediate_Hash[0] = 0x9B49BED3;
+			context->Intermediate_Hash[1] = 0x84B430FC;
+			context->Intermediate_Hash[2] = 0x6B8749CD;
+			context->Intermediate_Hash[3] = 0xEBFE5FE5;
+			context->Intermediate_Hash[4] = 0xD96E7393;
+			break;
+		case 12:
+       			context->Intermediate_Hash[0] = 0x01075307;
+		      	context->Intermediate_Hash[1] = 0xa2f1e037;
+			context->Intermediate_Hash[2] = 0x1186eeea;
+			context->Intermediate_Hash[3] = 0x88da9992;
+			context->Intermediate_Hash[4] = 0x168a5609;
+			break;
 	}
-	
-	if (version==10) {
-		context->Intermediate_Hash[0] = 0x76441D41;
-		context->Intermediate_Hash[1] = 0x4DE82659;
-		context->Intermediate_Hash[2] = 0x2E8EF85E;
-		context->Intermediate_Hash[3] = 0xB256FACA;
-		context->Intermediate_Hash[4] = 0xC4FE2DE8;
-	}
-
-	if (version==11) {
-		context->Intermediate_Hash[0] = 0x9B49BED3;
-		context->Intermediate_Hash[1] = 0x84B430FC;
-		context->Intermediate_Hash[2] = 0x6B8749CD;
-		context->Intermediate_Hash[3] = 0xEBFE5FE5;
-		context->Intermediate_Hash[4] = 0xD96E7393;
-	}
-
 	context->Length_Low  = 512;
 
 	return shaSuccess;
 }
-
-
 
 void HMAC_SHA1( unsigned char *result,
 		unsigned char *key, int key_length,
@@ -171,7 +179,7 @@ DWORD BootHddKeyGenerateEepromKeyData(
         
         // Static Version change not included yet
         
-	for (counter=9;counter<12;counter++)
+	for (counter=9;counter<13;counter++)
 	{
     		memset(&RC4_key,0,sizeof(rc4_key));
 		memcpy(&baEepromDataLocalCopy[0], pbEeprom_data, 0x30);
