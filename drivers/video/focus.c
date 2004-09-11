05c92506c33f8e40e0cc1052325a6a780a52c12d
David Pye <dmp@davidmpye.dyndns.org>
2004-09-11 21:39:58 +0000

    Sync with kernel driver. Thanks again to hamtitampti (Franz Lehner) for providing the Svideo cable used for this development.
    David

--- drivers/video/focus.c
+++ drivers/video/focus.c
@@ -12,8 +12,15 @@
  * VGA SoG/internal sync not yet implemented
 */
 #include "focus.h"
-#include "encoder.h"
+//#include "encoder.h"
 
+typedef struct _focus_pll_settings{
+	long dotclock;
+	int vga_htotal;
+	int vga_vtotal;
+	int tv_htotal;
+	int tv_vtotal;
+} focus_pll_settings;
 
 static const unsigned char focus_defaults[0xc4] = {
 	/*0x00*/ 0x00,0x00,0x00,0x00,0x80,0x02,0xaa,0x0a,
@@ -48,11 +55,6 @@ int focus_calc_hdtv_mode(
 	unsigned char pll_int,
 	unsigned char * regs
 	){
-	unsigned char b;
-	int m=0;
-	int tv_vtotal, tv_htotal, tv_vactive, tv_hactive, vga_htotal, vga_vtotal;
-	int pll_n, pll_m, pll_p, ncon, ncod;
-
 	memcpy(regs,focus_defaults,sizeof(focus_defaults));	
 	/* Uncomment for HDTV 480p colour bars */
 	//regs[0x0d]|=0x02;
@@ -67,7 +69,8 @@ int focus_calc_hdtv_mode(
 	
 	/*Set up video mode to HDTV, progressive, 
 	 * and disable YUV matrix bypass */
-	regs[0x92] = 0x1a;		
+	regs[0x92] = 0x1a;	
+	regs[0x93] &= ~0x40;
 	
 	switch (hdtv_mode) {
 		case HDTV_480p:
@@ -131,6 +134,9 @@ int focus_calc_hdtv_mode(
 			regs[0xA8] = 0x58;
 			regs[0xAA] = 0x8A;
 			regs[0xAC] = 0x4A;
+			/* HSync timing invert - needed to centre picture */
+			regs[0x93] |= 0x01;
+			
 			break;
 		case HDTV_1080i:
 			/* PLL settings */
@@ -173,12 +179,13 @@ int focus_calc_mode(xbox_video_mode * mode, struct riva_regs * riva_out)
 {
 	unsigned char b;
 	char* regs = riva_out->encoder_mode;
-	int m = 0;
 	int tv_htotal, tv_vtotal, tv_vactive, tv_hactive;
 	int vga_htotal, vga_vtotal;
-	long ncon, ncod;
-	int pll_m, pll_n, pll_p, vsc, hsc;
+	int vsc, hsc;
 
+	long dotclock;
+	focus_pll_settings pll_settings;
+	
 	memcpy(regs,focus_defaults,sizeof(focus_defaults));
 	
 	/* Uncomment for SDTV colour bars */
@@ -239,20 +246,24 @@ int focus_calc_mode(xbox_video_mode * mode, struct riva_regs * riva_out)
 	regs[0xA8] = 0xA0;
 	regs[0xAA] = 0xDB;
 	regs[0xAC] = 0x7E;
+
+	switch(mode->av_type) {
+		case AV_SVIDEO:
+			/* COMP_YUV - set to 1 to output YUV */	
+			regs[0x47] |= 0x04;
+			/* VID_MODE to 0 - SVIDEO */
+			regs[0x92] &= ~0x01;
+			break;
+		default:
+			/*Nothing as yet */
+			break;
+	}
 	
 	tv_vactive = tv_vactive * (1.0f-mode->voc);
 	vga_vtotal = mode->yres * ((float)tv_vtotal/tv_vactive);
 	vga_htotal = mode->xres * 1.25f;
 	tv_hactive = tv_hactive * (1.0f-mode->hoc);
-	/*These should be calculated or at least deduced.
-	 *However they are good enough for 640x480 -> 800x600 now*/
-	pll_n  = 32;
-	pll_m = 512;
-	pll_p = 4; 
-	
-	ncon = vga_htotal * vga_vtotal;
-	ncod = tv_htotal * tv_vtotal  * pll_p;
-	
+
 	regs[0x04] = (mode->xres+64)&0xFF;
 	regs[0x05] = ((mode->xres+64)>>8)&0xFF;
 
@@ -281,22 +292,21 @@ int focus_calc_mode(xbox_video_mode * mode, struct riva_regs * riva_out)
 		regs[0x09] = 0;
 	}
 
+	//PLL calculations
+	if (mode->tv_encoding==TV_ENC_NTSC) dotclock = vga_htotal * vga_vtotal * 59.94;
+	else dotclock = vga_htotal * vga_vtotal * 50;
 
-	regs[0x10] = (ncon)&0xFF;
-	regs[0x11] = (ncon>>8)&0xFF ;
-	regs[0x12] = (ncon>>16)&0xFF ;
-	regs[0x13] = (ncon>>24)&0xFF ;
-	regs[0x14] = (ncod)&0xFF ;
-	regs[0x15] = (ncod>>8)&0xFF ;
-	regs[0x16] = (ncod>>16)&0xFF ;
-	regs[0x17] = (ncod>>24)&0xFF ;
-	regs[0x18] = (pll_m-17)&0xFF;
-	regs[0x19] = ((pll_m-17)>>8)&0xFF;
-	regs[0x1A] = (pll_n-1)&0xFF ;
-	regs[0x1B] = ((pll_n-1)>>8)&0xFF ;
-	regs[0x1C] = (pll_p-1)&0xFF;
-	regs[0x1D] = (pll_p-1)&0xFF;
-	        
+	pll_settings.dotclock = dotclock;
+	pll_settings.vga_htotal = vga_htotal;
+	pll_settings.vga_vtotal = vga_vtotal;
+	pll_settings.tv_htotal = tv_htotal;
+	pll_settings.tv_vtotal = tv_vtotal;
+	
+	if (!focus_calc_pll_settings(&pll_settings,regs)) {
+		//Unable to calculate a valid PLL solution	
+		return 1;
+	}
+
 	/* Guesswork */
 	riva_out->ext.vsyncstart = vga_vtotal * 0.95;
 	riva_out->ext.hsyncstart = vga_htotal * 0.95;
@@ -322,3 +332,63 @@ int focus_calc_mode(xbox_video_mode * mode, struct riva_regs * riva_out)
 
 	return 1;
 }
+
+int focus_calc_pll_settings(focus_pll_settings *settings, char *regs) {
+        int m, n, p;
+	long dotclock = (*settings).dotclock;
+	int pll_multiplier;
+	long ncon, ncod;
+	
+	ncon = (*settings).vga_htotal * (*settings).vga_vtotal;
+
+	//Multipliers between 1 and 6 are the limit as output clock cant be >150MHz
+	//The lower the multiplier, the more stable the PLL (theoretically)
+	for (pll_multiplier=4; pll_multiplier<6; pll_multiplier++) {
+		long nco_out_clk;
+		ncod = (*settings).tv_htotal * (*settings).tv_vtotal * pll_multiplier;
+		//NCO output clock is the reference clock (27MHz) multiplied by
+		//the ncon/ncod fraction.
+		nco_out_clk = 27000000*(ncon/(float)ncod);
+		for (n=2; n<270;++n) {
+			//PLL input clock is NCO output clock divided by N
+			//Valid range is 100kHz to 1000kHz
+			long pll_in_clk = nco_out_clk/n;
+			if ( pll_in_clk >=100000 && pll_in_clk <=1000000) {
+				for (m=2; m<3000;++m) {
+					//PLL output clock is PLL input clock multiplied
+					//by M. Valid range is 100MHz to 300MHz
+					long pll_out_clk = pll_in_clk * m;
+					if (pll_out_clk >=100000000 && pll_out_clk <= 300000000) {
+						//Output clocks are PLL output clock divided by P.
+						//Valid range is anything LESS than 150MHz, but
+						//it must match the incoming pixel clock rate.
+						long output_clk = pll_out_clk/pll_multiplier;
+						if (output_clk == dotclock || output_clk+1 == dotclock || output_clk-1 == dotclock) {
+							//Got it - the pll is now correctly aligned
+							//Set up the PLL registers
+							regs[0x10] = (ncon)&0xFF;
+							regs[0x11] = (ncon>>8)&0xFF ;
+							regs[0x12] = (ncon>>16)&0xFF ;
+							regs[0x13] = (ncon>>24)&0xFF ;
+							regs[0x14] = (ncod)&0xFF ;
+							regs[0x15] = (ncod>>8)&0xFF ;
+							regs[0x16] = (ncod>>16)&0xFF ;
+							regs[0x17] = (ncod>>24)&0xFF ;
+
+							regs[0x18] = (m-17)&0xFF;
+							regs[0x19] = ((m-17)>>8)&0xFF;
+							regs[0x1A] = (n-1)&0xFF ;
+							regs[0x1B] = ((n-1)>>8)&0xFF ;
+							regs[0x1C] = (pll_multiplier-1)&0xFF;
+							regs[0x1D] = (pll_multiplier-1)&0xFF;
+							return 1;
+						}
+					}
+				}
+			}
+		}	
+	}
+	//Seems no valid solution was possible 
+	return 0;
+}
+
