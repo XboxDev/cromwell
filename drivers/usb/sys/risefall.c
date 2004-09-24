8de5d3da439a0e04a5254630698137f1d76b8e62
David Pye <dmp@davidmpye.dyndns.org>
2004-09-24 19:48:08 +0000

    Remove gotos and restructure. Please test with usb keyboards, and remotes.

--- drivers/usb/sys/risefall.c
+++ drivers/usb/sys/risefall.c
@@ -24,131 +24,88 @@ int risefall_xpad_BUTTON(unsigned char selected_Button) {
 	// Section Keyboard	
 	
 	if (current_keyboard_key!=0) {
-			int returnvalue=0;
-			
-			switch (selected_Button) {
-				case TRIGGER_XPAD_KEY_A :
-			   				   if (current_keyboard_key ==  0x28) returnvalue = 1; 
-							   break;
-				case TRIGGER_XPAD_KEY_B :
-			   				   if (current_keyboard_key ==  0x29) returnvalue = 1; 
-							   break;
-				case TRIGGER_XPAD_PAD_UP :
-							   if (current_keyboard_key == 0x52) returnvalue = 1; 
-							   break;
-				case TRIGGER_XPAD_PAD_DOWN :
-							   if (current_keyboard_key == 0x51) returnvalue = 1; 
-							   break;
-				case TRIGGER_XPAD_PAD_LEFT :
-							   if (current_keyboard_key == 0x50) returnvalue = 1; 
-							   break;
-				case TRIGGER_XPAD_PAD_RIGHT :
-							   if (current_keyboard_key == 0x4f) returnvalue = 1; 
-							   break;
-                        }
-                        if (returnvalue!=0) {
-                		current_keyboard_key = 0;
-                		return returnvalue;
-                			
-                	}             
+		switch (selected_Button) {
+			case TRIGGER_XPAD_KEY_A :
+		   		if (current_keyboard_key == 0x28) {
+					current_keyboard_key=0;
+					return 1;
+				}
+			case TRIGGER_XPAD_KEY_B :
+		   		if (current_keyboard_key == 0x29) {
+					current_keyboard_key=0;
+					return 1;
+				}
+			case TRIGGER_XPAD_PAD_UP :
+		   		if (current_keyboard_key == 0x52) {
+					current_keyboard_key=0;
+					return 1;
+				}
+			case TRIGGER_XPAD_PAD_DOWN :
+		   		if (current_keyboard_key == 0x51) {
+					current_keyboard_key=0;
+					return 1;
+				}
+			case TRIGGER_XPAD_PAD_LEFT :
+		   		if (current_keyboard_key == 0x50) {
+					current_keyboard_key=0;
+					return 1;
+				}
+			case TRIGGER_XPAD_PAD_RIGHT :
+		   		if (current_keyboard_key == 0x4f) {
+					current_keyboard_key=0;
+					return 1;
+				}
+		}
 	}
-
 	// Section for IR Driver   
 
        temp = current_remote_keydir;
         
        if ((temp&0x100)==0x100) {
        		int counter;
-		
 		for (counter = 0; counter < (sizeof(accepted_xremote)) ; counter++) {
-                        	
 			if (accepted_xremote[counter] == current_remote_key) {
-					goto ProcessKeyFound;
+                		temp &= 0xff;
+	                       	current_remote_key &=0xff;
+				switch (selected_Button) {
+					case TRIGGER_XPAD_KEY_A :
+				   		if (current_remote_key ==  0xb) return 1; 
+					case TRIGGER_XPAD_PAD_UP :
+						if (current_remote_key == 0xa6) return 1; 
+					case TRIGGER_XPAD_PAD_DOWN :
+						if (current_remote_key == 0xa7) return 1; 
+					case TRIGGER_XPAD_PAD_LEFT :
+						if (current_remote_key == 0xa9) return 1; 
+					case TRIGGER_XPAD_PAD_RIGHT :
+						if (current_remote_key == 0xa8) return 1; 
+					case TRIGGER_XPAD_PAD_KEYSTROKE :
+						if (current_remote_key == 0xd5) return 0xd5; 
+						if ((current_remote_key>0xc5)&(current_remote_key<0xd0)) return current_remote_key;
 				}
-		}
-                // oh, key not even handled ? .. we clear the keystroke event
-		
-		current_remote_keydir &=0xff;
-		goto ProcessXPAD;               
-                
-                ProcessKeyFound:
-                
-                temp &= 0xff;
-
-		if  ( 1==1 ){	
-                        int returnvalue;
-	                
-                        current_remote_key &=0xff;
-                        
-			returnvalue = 0;
-			
-			switch (selected_Button) {
-				case TRIGGER_XPAD_KEY_A :
-			   				   if (current_remote_key ==  0xb) returnvalue = 1; 
-							   break;
-				case TRIGGER_XPAD_PAD_UP :
-							   if (current_remote_key == 0xa6) returnvalue = 1; 
-							   break;
-				case TRIGGER_XPAD_PAD_DOWN :
-							   if (current_remote_key == 0xa7) returnvalue = 1; 
-							   break;
-				case TRIGGER_XPAD_PAD_LEFT :
-							   if (current_remote_key == 0xa9) returnvalue = 1; 
-							   break;
-				case TRIGGER_XPAD_PAD_RIGHT :
-							   if (current_remote_key == 0xa8) returnvalue = 1; 
-							   break;
-				case TRIGGER_XPAD_PAD_KEYSTROKE :
-							   if (current_remote_key == 0xd5) returnvalue = 0xd5; 
-							   if ((current_remote_key>0xc5)&(current_remote_key<0xd0)) returnvalue = current_remote_key;
-							   break;
+				return 0;
 			}
-                   	
-                        if (returnvalue!=0) {
-                        	current_remote_keydir &=0xff;
-                        	return returnvalue;	
-                        } else {
-                        	return 0;	
-                        }
-
 		}
-		
 	}
         
-ProcessXPAD:
 	for (xpad_id=0;xpad_id<xpad_num; xpad_id++) {
-	
-
         	// We continue with normal XPAD operations
         	if (selected_Button < 6) {
         	
-        		int Button_actual=0;
         		unsigned char Button;
         	
         		Button = XPAD_current[xpad_id].keys[selected_Button];
-       		 
-		        if (Button==0x0)
-		        {
-		              	Button_actual = 0;	
-		        }
-		        
-			if (Button>0x30)
-			{
-				Button_actual = 1;
-			}
 		
-			if ((Button_actual==1)&(xpad_button_history[selected_Button]==0)) {
+			if ((Button>0x30)&&(xpad_button_history[selected_Button]==0)) {
 				// Button Rising Edge
-				xpad_button_history[selected_Button] = Button_actual;		
+				xpad_button_history[selected_Button] = (Button>0x30);		
 				return 1;
 			}	
 			
-			if ((Button_actual==0)&(xpad_button_history[selected_Button]==1)) {
+			if ((Button==0x00)&&(xpad_button_history[selected_Button]==1)) {
 				// Button Falling Edge
-				xpad_button_history[selected_Button] = Button_actual;		
+				xpad_button_history[selected_Button] = (Button==0x00);		
 				return -1;
 			}	
-			//return 0; 
 		}
  		
  		if ((selected_Button > 5) & (selected_Button < 10) ) {
