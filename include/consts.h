bd38d268f701ba2aa6da8279b6e24b1ad3d3c852
David Pye <dmp@davidmpye.dyndns.org>
2004-10-23 19:39:04 +0000

    Prepare for 2.32 release.

--- include/consts.h
+++ include/consts.h
@@ -16,7 +16,7 @@
  *                                                                         *
  ***************************************************************************/
 
-#define VERSION "2.32-dev"
+#define VERSION "2.32"
 
 #define PCI_CFG_ADDR 0x0CF8
 #define PCI_CFG_DATA 0x0CFC
