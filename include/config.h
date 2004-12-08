d9bfc29a13ae1f4a7473cd7dcbc18976c023a6fb
David Pye <dmp@davidmpye.dyndns.org>
2004-12-08 21:51:21 +0000

    2.40

--- include/config.h
+++ include/config.h
@@ -1,7 +1,7 @@
 ////////////////////// compile-time options ////////////////////////////////
 
 //Cromwell version number
-#define VERSION "2.40-rc1"
+#define VERSION "2.40"
 
 // selects between the supported video modes, see boot.h for enum listing those available
 //#define VIDEO_PREFERRED_MODE VIDEO_MODE_800x600
