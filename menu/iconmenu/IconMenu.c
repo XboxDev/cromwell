750415bfe182e701aa97bce332e1310115ea556b
David Pye <dmp@davidmpye.dyndns.org>
2005-01-08 02:08:56 +0000

    Fix odd condition that timeout timer could remain active, when it shouldn't have been

--- menu/iconmenu/IconMenu.c
+++ menu/iconmenu/IconMenu.c
@@ -172,24 +172,29 @@ void IconMenu(void) {
 			}
 			temp=0;
 		}
-		//If anybody has toggled the xpad left/right, disable the timeout.
-		if (temp!=0) {
-			temp = IoInputDword(0x8008) - COUNT_start;
-		}
 		
 		if ((risefall_xpad_BUTTON(TRIGGER_XPAD_KEY_A) == 1) || risefall_xpad_BUTTON(TRIGGER_XPAD_KEY_START) == 1 || 
 				(u32)(temp>(0x369E99*BOOT_TIMEWAIT))) {
 			memcpy((void*)FB_START,videosavepage,FB_SIZE);
 			VIDEO_CURSOR_POSX=nTempCursorResumeX;
 			VIDEO_CURSOR_POSY=nTempCursorResumeY;
+		
+			//If we didn't time out, disable the timeout now.
+			if (temp<(0x369E99*BOOT_TIMEWAIT)) temp=0;
+			else timedOut=1;
 			
-			if (temp>(0x369E99*BOOT_TIMEWAIT)) timedOut=1;
 			//Icon selected - invoke function pointer.
 			if (selectedIcon->functionPtr!=NULL) selectedIcon->functionPtr(selectedIcon->functionDataPtr);
 			//If we come back to this menu, make sure we are redrawn, and that we replace the saved video page
 			changed=1;
 			memcpy((void*)FB_START,videosavepage,FB_SIZE);
 		}
+
+		//If we got any events, disable the timout timer.
+		if (temp!=0) {
+			temp = IoInputDword(0x8008) - COUNT_start;
+		}
+
 		if (changed) {
 			BootVideoClearScreen(&jpegBackdrop, nTempCursorY, VIDEO_CURSOR_POSY+1);
 			IconMenuDraw(nModeDependentOffset, nTempCursorY);
