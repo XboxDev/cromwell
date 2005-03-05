d977e85dbacf20b0386a61a49e8f4a62cd7178f4
Thomas Pedley <me@shallax.com>
2005-03-05 00:11:37 +0000

    * Shrunk icon menu to 3 icons. * Added left/right "scrolling" arrows to the icon menu. * Added left/right arrows to the backdrop.
    Comments welcome.

--- menu/iconmenu/IconMenu.c
+++ menu/iconmenu/IconMenu.c
@@ -35,6 +35,7 @@
 ICON *firstIcon=NULL;
 ICON *selectedIcon=NULL;
 ICON *firstVisibleIcon=NULL;
+ICON *lastVisibleIcon=NULL;
 int timedOut=0;
 
 void AddIcon(ICON *newIcon) {
@@ -59,13 +60,14 @@ void AddIcon(ICON *newIcon) {
 static void IconMenuDraw(int nXOffset, int nYOffset) {
 	ICON *iconPtr;
 	int iconcount;
-	
+	u8 opaqueness;
+
 	if (firstVisibleIcon==NULL) firstVisibleIcon = firstIcon;
 	if (selectedIcon==NULL) selectedIcon = firstIcon;
 	iconPtr = firstVisibleIcon;
-	//There are max four 'bays' for displaying icons in - we only draw the four.
-	for (iconcount=0; iconcount<4; iconcount++) {
-		u8 opaqueness;
+
+	//There are max 3 (three) 'bays' for displaying icons in - we only draw the 3.
+	for (iconcount=0; iconcount<3; iconcount++) {
 		if (iconPtr==NULL) {
 			//No more icons to draw
 			return;
@@ -74,7 +76,7 @@ static void IconMenuDraw(int nXOffset, int nYOffset) {
 			//Selected icon has less transparency
 			//and has a caption drawn underneath it
 			opaqueness = SELECTED;
-			VIDEO_CURSOR_POSX=nXOffset+112*(iconcount+1)*4;
+			VIDEO_CURSOR_POSX=nXOffset+140*(iconcount+1)*4;
 			VIDEO_CURSOR_POSY=nYOffset+20;
 			VIDEO_ATTR=0xffffff;
 			printk("%s\n",iconPtr->szCaption);
@@ -82,30 +84,60 @@ static void IconMenuDraw(int nXOffset, int nYOffset) {
 		else opaqueness = TRANSPARENTNESS;
 		
 		BootVideoJpegBlitBlend(
-			(u8 *)(FB_START+((vmode.width * (nYOffset-74))+nXOffset+(112*(iconcount+1))) * 4),
+			(u8 *)(FB_START+((vmode.width * (nYOffset-74))+nXOffset+(140*(iconcount+1))) * 4),
 			vmode.width, // dest bytes per line
 			&jpegBackdrop, // source jpeg object
 			(u8 *)(jpegBackdrop.pData+(iconPtr->iconSlot * jpegBackdrop.bpp)),
 			0xff00ff|(((u32)opaqueness)<<24),
-			(u8 *)(jpegBackdrop.pBackdrop + ((jpegBackdrop.width * (nYOffset-74)) + nXOffset+(112*(iconcount+1))) * jpegBackdrop.bpp),
+			(u8 *)(jpegBackdrop.pBackdrop + ((jpegBackdrop.width * (nYOffset-74)) + nXOffset+(140*(iconcount+1))) * jpegBackdrop.bpp),
 			ICON_WIDTH, ICON_HEIGHT
 		);
+		lastVisibleIcon = iconPtr;
 		iconPtr = iconPtr->nextIcon;
+	}
+
+	// If there is an icon off screen to the left, draw this icon.
+	if(firstVisibleIcon->previousIcon != NULL) {
+		//opaqueness = TRANSPARENTNESS;
+		opaqueness = SELECTED;
+		BootVideoJpegBlitBlend(
+			(u8 *)(FB_START+((vmode.width * (nYOffset-74))+nXOffset+(50)) * 4),
+			vmode.width, // dest bytes per line
+			&jpegBackdrop, // source jpeg object
+			(u8 *)(jpegBackdrop.pData+(ICON_WIDTH*5 * jpegBackdrop.bpp)),
+			0xff00ff|(((u32)opaqueness)<<24),
+			(u8 *)(jpegBackdrop.pBackdrop + ((jpegBackdrop.width * (nYOffset-74)) + nXOffset+(50)) * jpegBackdrop.bpp),
+			ICON_WIDTH, ICON_HEIGHT
+		);
+	}
+
+	// If there is an icon off screen to the right, draw this icon.
+	if(lastVisibleIcon->nextIcon != NULL) {
+		//opaqueness = TRANSPARENTNESS;
+		opaqueness = SELECTED;
+		BootVideoJpegBlitBlend(
+			(u8 *)(FB_START+((vmode.width * (nYOffset-74))+nXOffset+(510)) * 4),
+			vmode.width, // dest bytes per line
+			&jpegBackdrop, // source jpeg object
+			(u8 *)(jpegBackdrop.pData+(ICON_WIDTH*6 * jpegBackdrop.bpp)),
+			0xff00ff|(((u32)opaqueness)<<24),
+			(u8 *)(jpegBackdrop.pBackdrop + ((jpegBackdrop.width * (nYOffset-74)) + nXOffset+(510)) * jpegBackdrop.bpp),
+			ICON_WIDTH, ICON_HEIGHT
+		);
 	}
 }
 
 void IconMenu(void) {
-        unsigned char *videosavepage;
+	unsigned char *videosavepage;
         
-        u32 COUNT_start;
-        u32 temp=1;
+	u32 COUNT_start;
+	u32 temp=1;
 	ICON *iconPtr=NULL;
 
 	extern int nTempCursorMbrX, nTempCursorMbrY; 
 	int nTempCursorResumeX, nTempCursorResumeY ;
 	int nTempCursorX, nTempCursorY;
 	int nModeDependentOffset=(vmode.width-640)/2;  
-
 	
 	nTempCursorResumeX=nTempCursorMbrX;
 	nTempCursorResumeY=nTempCursorMbrY;
@@ -120,16 +152,16 @@ void IconMenu(void) {
 	VIDEO_CURSOR_POSX=((252+nModeDependentOffset)<<2);
 	VIDEO_CURSOR_POSY=nTempCursorY-100;
 
-#ifndef SILENT_MODE
+//#ifndef SILENT_MODE
 	//In silent mode, don't draw the menu the first time.
 	//If we get a left/right xpad event, it will be registered,
 	//and the menu will 'appear'. Otherwise, it will proceed quietly
 	//and boot the default boot item
 	VIDEO_ATTR=0xffc8c8c8;
-	printk("Select from Menu\n");
+	//printk("Select from Menu\n");
 	VIDEO_ATTR=0xffffffff;
 	IconMenuDraw(nModeDependentOffset, nTempCursorY);
-#endif
+//#endif
 	COUNT_start = IoInputDword(0x8008);
 	//Main menu event loop.
 	while(1)
@@ -141,18 +173,23 @@ void IconMenu(void) {
 			if (selectedIcon->nextIcon!=NULL) {
 				//A bit ugly, but need to find the last visible icon, and see if 
 				//we are moving further right from it.
-				ICON *lastVisibleIcon=firstVisibleIcon;
+				lastVisibleIcon=firstVisibleIcon;
 				int i=0;
-				for (i=0; i<3; i++) {
-					if (lastVisibleIcon->nextIcon==NULL) break;
+				for (i=0; i<2; i++) {
+					if (lastVisibleIcon->nextIcon==NULL) {
+						break;
+					}
 					lastVisibleIcon = lastVisibleIcon->nextIcon;
 				}
-				if (selectedIcon == lastVisibleIcon) { 
+				if (selectedIcon == lastVisibleIcon) {
 					//We are moving further right, so slide all the icons along. 
-					firstVisibleIcon = firstVisibleIcon->nextIcon;	
+					if(lastVisibleIcon->nextIcon != NULL) {
+						firstVisibleIcon = firstVisibleIcon->nextIcon;	
+					}
 					//As all the icons have moved, we need to refresh the entire page.
 					memcpy((void*)FB_START,videosavepage,FB_SIZE);
 				}
+				memcpy((void*)FB_START,videosavepage,FB_SIZE);
 				selectedIcon = selectedIcon->nextIcon;
 				changed=1;
 			}
@@ -163,7 +200,9 @@ void IconMenu(void) {
 			if (selectedIcon->previousIcon!=NULL) {
 				if (selectedIcon == firstVisibleIcon) {
 					//We are moving further left, so slide all the icons along. 
-					firstVisibleIcon = selectedIcon->previousIcon;
+					if(firstVisibleIcon->previousIcon != NULL) {
+						firstVisibleIcon = firstVisibleIcon->previousIcon;
+					}
 					//As all the icons have moved, we need to refresh the entire page.
 					memcpy((void*)FB_START,videosavepage,FB_SIZE);
 				}
@@ -172,29 +211,24 @@ void IconMenu(void) {
 			}
 			temp=0;
 		}
+		//If anybody has toggled the xpad left/right, disable the timeout.
+		if (temp!=0) {
+			temp = IoInputDword(0x8008) - COUNT_start;
+		}
 		
 		if ((risefall_xpad_BUTTON(TRIGGER_XPAD_KEY_A) == 1) || risefall_xpad_BUTTON(TRIGGER_XPAD_KEY_START) == 1 || 
 				(u32)(temp>(0x369E99*BOOT_TIMEWAIT))) {
 			memcpy((void*)FB_START,videosavepage,FB_SIZE);
 			VIDEO_CURSOR_POSX=nTempCursorResumeX;
 			VIDEO_CURSOR_POSY=nTempCursorResumeY;
-		
-			//If we didn't time out, disable the timeout now.
-			if (temp<(0x369E99*BOOT_TIMEWAIT)) temp=0;
-			else timedOut=1;
 			
+			if (temp>(0x369E99*BOOT_TIMEWAIT)) timedOut=1;
 			//Icon selected - invoke function pointer.
 			if (selectedIcon->functionPtr!=NULL) selectedIcon->functionPtr(selectedIcon->functionDataPtr);
 			//If we come back to this menu, make sure we are redrawn, and that we replace the saved video page
 			changed=1;
 			memcpy((void*)FB_START,videosavepage,FB_SIZE);
 		}
-
-		//If we got any events, disable the timout timer.
-		if (temp!=0) {
-			temp = IoInputDword(0x8008) - COUNT_start;
-		}
-
 		if (changed) {
 			BootVideoClearScreen(&jpegBackdrop, nTempCursorY, VIDEO_CURSOR_POSY+1);
 			IconMenuDraw(nModeDependentOffset, nTempCursorY);
